89eb071272a3dfd6d32b68891e6005463f7bcc3e
El Houcine Qara <elhoucine.qara@harx.ai>
2025-07-29 06:46:35 +0100

    Fix : Nice to have

--- src/components/Logo.tsx
+++ src/components/Logo.tsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import harxLogo from '../assets/harx-blanc.jpg';
 
 interface LogoProps {
   className?: string;
@@ -8,7 +9,7 @@ const Logo: React.FC<LogoProps> = ({ className = "" }) => {
   return (
     <div className={`overflow-hidden mb-1 mt-0 mx-auto inline-block ${className}`}>
       <img 
-        src="/src/assets/harx-blanc.jpg" 
+        src={harxLogo} 
         alt="HARX Logo" 
         className="md:w-80 md:h-[7rem] object-contain rounded-2xl"
         style={{ borderRadius: '15%' }}
